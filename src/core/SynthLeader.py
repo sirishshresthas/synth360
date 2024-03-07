281f717e066d6fac271a3b3873c2f34fc26762d8
Sirish Shrestha <shresthas@ccl.org>
2024-03-07 00:01:10 +0000

    added visualization method in the class

--- src/core/SynthLeader.py
+++ src/core/SynthLeader.py
@@ -1,12 +1,16 @@
-import pandas as pd
-from typing import Any, List, Dict
-import numpy as np
 import os
+import math
 import torch
+import numpy as np
+import pandas as pd
+from typing import Any, List, Dict, Optional, Union
 from sdv.metadata import SingleTableMetadata
 from sdv.single_table import GaussianCopulaSynthesizer, CTGANSynthesizer, CopulaGANSynthesizer
 from sdv.evaluation.single_table import run_diagnostic, evaluate_quality
 from src.core.utilities import globals
+from sdv.evaluation.single_table import get_column_plot
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 
 class SynthLeader(object):
@@ -62,8 +66,31 @@ class SynthLeader(object):
                 computer_representation='Float'
             )
 
+    def train_synthesizer(self, Model: CopulaGANSynthesizer | CTGANSynthesizer | GaussianCopulaSynthesizer, model_name: str = '', force: bool = False, params: Dict[str, Any] = {}):
+        if model_name != '':
+            model_name = str(globals.DATA_DIR / model_name)
+
+        if os.path.exists(model_name):
+            if force:
+                synthesizer = Model(**params)
+
+            else:
+
+                synthesizer = Model.load(
+                    filepath=model_name)
+
+        else:
+            synthesizer = Model(**params)
+
+        synthesizer.fit(self.df)
+
+        if model_name:
+            synthesizer.save(filepath=model_name)
+
+        return synthesizer
+
     # Train Gaussian Copula
-    def train_copula_synthesizer(self, model_name: str = '', force=False,  enforce_min_max_values=True, enforce_rounding=True, numerical_distributions={}, default_distribution: str = 'norm'):
+    def train_copula_synthesizer(self, model_name: str = '', force=False,  enforce_min_max_values=True, enforce_rounding=True, numerical_distributions: Optional[Dict] = None, default_distribution: str = 'norm'):
 
         params = {
             "metadata": self.metadata,
@@ -73,29 +100,12 @@ class SynthLeader(object):
             "default_distribution": default_distribution
         }
 
-        if model_name:
-            model_name = str(globals.DATA_DIR / model_name)
-
-        if os.path.exists(model_name):
-            if force:
-                synthesizer = GaussianCopulaSynthesizer(**params)
-
-            else:
-
-                synthesizer = GaussianCopulaSynthesizer.load(
-                    filepath=model_name)
-
-        else:
-            synthesizer = GaussianCopulaSynthesizer(**params)
-
-        synthesizer.fit(self.df)
-
-        if model_name:
-            synthesizer.save(filepath=model_name)
+        synthesizer: GaussianCopulaSynthesizer = self.train_synthesizer(
+            Model=GaussianCopulaSynthesizer, model_name=model_name, force=force, params=params)
 
         return synthesizer
 
-    def train_ctgan_synthesizer(self, model_name: str = '', epochs=500, enforce_rounding=True, enforce_min_max_values=True, verbose=False, force=False):
+    def train_ctgan_synthesizer(self, model_name: str = '', epochs=500, enforce_rounding=True, enforce_min_max_values=True, verbose=True, force=False):
 
         params = {
             "metadata": self.metadata,
@@ -106,23 +116,8 @@ class SynthLeader(object):
             "cuda": torch.cuda.is_available()
         }
 
-        if model_name:
-            model_name = str(globals.DATA_DIR / model_name)
-
-        if os.path.exists(model_name):
-
-            if force:
-                synthesizer = CTGANSynthesizer(**params)
-            else:
-                synthesizer = CTGANSynthesizer.load(filepath=model_name)
-
-        else:
-            synthesizer = CTGANSynthesizer(**params)
-
-        synthesizer.fit(self.df)
-
-        if model_name:
-            synthesizer.save(filepath=model_name)
+        synthesizer: CTGANSynthesizer = self.train_synthesizer(
+            Model=CTGANSynthesizer, model_name=model_name, force=force, params=params)
 
         return synthesizer
 
@@ -131,7 +126,7 @@ class SynthLeader(object):
         # create autoencoder model with demographics
         pass
 
-    def train_copula_gan_synthesizer(self, model_name: str = '', enforce_min_max_value: bool = True, enforce_rounding: bool = False, numerical_distributions: Dict = None, epochs: int = 500, verbose: bool = True):
+    def train_copula_gan_synthesizer(self, model_name: str = '', enforce_min_max_value: bool = True, enforce_rounding: bool = False, numerical_distributions: Dict[str, Any] = {}, epochs: int = 500, verbose: bool = True, force: bool = False):
 
         params = {
 
@@ -142,6 +137,11 @@ class SynthLeader(object):
             "epochs": epochs,
             "verbose": verbose
         }
+
+        synthesizer: CopulaGANSynthesizer = self.train_synthesizer(
+            Model=CopulaGANSynthesizer, model_name=model_name, force=force, params=params)
+
+        return synthesizer
 
     def gan_hyperparameter_tuning(self):
         # todo:
@@ -169,6 +169,35 @@ class SynthLeader(object):
         )
 
         return quality
+
+    def visualize_data(self, synthetic_data, column_names: str | List):
+
+        if isinstance(column_names, str):
+            column_names = [column_names]
+
+        total_plots = len(column_names)
+        rows = math.ceil(math.sqrt(total_plots))
+        cols = math.ceil(total_plots / rows)
+
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=column_names)
+
+        for i, c in enumerate(column_names):
+            column_fig = get_column_plot(
+                    real_data=self.df,
+                    synthetic_data=synthetic_data,
+                    column_name=c,
+                    metadata=self.metadata
+                )
+            
+            for trace in column_fig.data:
+                fig.add_trace(
+                    trace,
+                    row=(i // cols) + 1,
+                    col=(i % cols) + 1
+                )
+
+        fig.update_layout(height=rows * 300, width=cols * 300, title_text="Column Plots")
+        fig.show()
 
     def get_corr_diff(self, corr_a, corr_b):
 
