db6227f85528227179ea03063df517c448ffe3ae
Sirish Shrestha <shresthas@ccl.org>
2024-03-26 00:32:32 +0000

    code cleanup

--- src/core/SynthLeader.py
+++ src/core/SynthLeader.py
@@ -11,18 +11,29 @@ from src.core.utilities import globals
 from sdv.evaluation.single_table import get_column_plot
 import plotly.io as pio
 from plotly.subplots import make_subplots
-from hyperopt import hp, STATUS_OK, fmin, tpe, Trials
+# from hyperopt import hp, STATUS_OK, fmin, tpe, Trials
 import plotly.graph_objects as go
 import plotly.io as pio
+from pathlib import Path
 
 
 class SynthLeader(object):
 
     def __init__(self, df: pd.DataFrame, name: str = ''):
         self.df = df
-        self._metadata: SingleTableMetadata = self.create_metadata()
         self.name = name
-        print(f"Cuda: {torch.cuda.is_available()}")
+
+        self.data_dir = Path("./data/")
+        self.sub_dirs = ['models', 'figures', 'metadata']
+
+        for sub_dir in self.sub_dirs:
+            dir_path = self.data_dir / sub_dir
+            dir_path.mkdir(parents=True, exist_ok=True)
+            setattr(self, f"{sub_dir}_dir", dir_path)
+
+        self._metadata: SingleTableMetadata = self.create_metadata()
+        print(
+            f"GPU: {torch.cuda.is_available() or torch.backends.mps.is_available()}")
 
     def __repr__(self):
         """
@@ -37,6 +48,15 @@ class SynthLeader(object):
     @metadata.setter
     def metadata(self, metadata: SingleTableMetadata) -> None:
         self._metadata = metadata
+
+    def create_metadata(self) -> SingleTableMetadata:
+        metadata: SingleTableMetadata = SingleTableMetadata()
+
+        metadata.detect_from_dataframe(self.df)
+        metadata.validate()
+        # metadata.save_to_json(filepath=self.metadata_dir / "metadata.json")
+
+        return metadata
 
     def _get_numeric_cols(self) -> List[str]:
         cols = self.df.select_dtypes(
@@ -57,13 +77,6 @@ class SynthLeader(object):
 
         return styled_matrix
 
-    def create_metadata(self) -> SingleTableMetadata:
-        metadata: SingleTableMetadata = SingleTableMetadata()
-        metadata.detect_from_dataframe(self.df)
-        metadata.validate()
-
-        return metadata
-
     def update_metadata(self, cols: List = [], sdtype: str = 'numerical'):
 
         for c in cols:
@@ -74,16 +87,18 @@ class SynthLeader(object):
             )
 
     def train_synthesizer(self, Model: CopulaGANSynthesizer | CTGANSynthesizer | GaussianCopulaSynthesizer, model_name: str = '', force: bool = False, params: Dict[str, Any] = {}):
+
         if model_name != '':
-            model_name = str(globals.DATA_DIR / model_name)
+            model_name = str(self.models_dir / model_name)
 
         if not os.path.exists(model_name) or force:
-            print("Retraining model")
+            print(
+                "Training model .. this may take several minutes or hours depending on the system.")
             synthesizer = Model(**params)
             synthesizer.fit(self.df)
 
         else:
-            print("Loading existing model")
+            print("Retrieving existing model")
             synthesizer = Model.load(
                 filepath=model_name)
 
@@ -108,10 +123,10 @@ class SynthLeader(object):
         return synthesizer
 
     def train_ctgan_synthesizer(self, model_name: str = '', epochs=500, enforce_rounding=True, enforce_min_max_values=True, verbose=True, force=False, batch_size: int = 512):
-        
+
         pac: int = 10
-        
-        for diviser in (10, 8): 
+
+        for diviser in (10, 8):
             if batch_size % diviser == 0:
                 pac = diviser
                 break
@@ -139,10 +154,10 @@ class SynthLeader(object):
         pass
 
     def train_copula_gan_synthesizer(self, model_name: str = '', enforce_min_max_value: bool = True, enforce_rounding: bool = False, numerical_distributions: Dict[str, Any] = {}, epochs: int = 500, verbose: bool = True, force: bool = False, batch_size: int = 512):
-        
+
         pac: int = 10
-        
-        for diviser in (10, 8): 
+
+        for diviser in (10, 8):
             if batch_size % diviser == 0:
                 pac = diviser
                 break
@@ -154,8 +169,8 @@ class SynthLeader(object):
             "enforce_rounding": enforce_rounding,
             "numerical_distributions": numerical_distributions,
             "epochs": epochs,
-            "verbose": verbose, 
-            "batch_size": batch_size, 
+            "verbose": verbose,
+            "batch_size": batch_size,
             "pac": pac
         }
 
@@ -164,57 +179,57 @@ class SynthLeader(object):
 
         return synthesizer
 
-    def tune_hyperparameters(self,
-                             epochs: List = [500, 700, 1000],
-                             batch_size: List = [128, 256],
-                             generator_dim: List = [
-                                 (64, 64), (128, 128), (256, 256)],
-                             discriminator_dim: List = [
-                                 (64, 64), (128, 128), (256, 256)],
-                             discriminator_decay=[0, 0.0001, 0.001, 0.01, 0.1]
-                             ):
+#     def tune_hyperparameters(self,
+#                              epochs: List = [500, 700, 1000],
+#                              batch_size: List = [128, 256],
+#                              generator_dim: List = [
+#                                  (64, 64), (128, 128), (256, 256)],
+#                              discriminator_dim: List = [
+#                                  (64, 64), (128, 128), (256, 256)],
+#                              discriminator_decay=[0, 0.0001, 0.001, 0.01, 0.1]
+#                              ):
 
-        space = {
-            'epochs': hp.choice('epochs', epochs),
-            # this goes from e^-10 to e^-1
-            'generator_lr': hp.loguniform('generator_lr', -10, -1),
-            'discriminator_lr': hp.loguniform('discriminator_lr', -10, -1),
-            'generator_dim': hp.choice('generator_dim', generator_dim),
-            'discriminator_dim': hp.choice('discriminator_dim', discriminator_dim),
-            'discriminator_decay': hp.choice('discriminator_decay', discriminator_decay),
-            'batch_size': hp.choice('batch_size', batch_size)
-        }
+#         space = {
+#             'epochs': hp.choice('epochs', epochs),
+#             # this goes from e^-10 to e^-1
+#             'generator_lr': hp.loguniform('generator_lr', -10, -1),
+#             'discriminator_lr': hp.loguniform('discriminator_lr', -10, -1),
+#             'generator_dim': hp.choice('generator_dim', generator_dim),
+#             'discriminator_dim': hp.choice('discriminator_dim', discriminator_dim),
+#             'discriminator_decay': hp.choice('discriminator_decay', discriminator_decay),
+#             'batch_size': hp.choice('batch_size', batch_size)
+#         }
 
-        trials = Trials()
-        best = fmin(fn=self.objective,
-                    space=space,
-                    algo=tpe.suggest,
-                    max_evals=50,
-                    trials=trials)
+#         trials = Trials()
+#         best = fmin(fn=self.objective,
+#                     space=space,
+#                     algo=tpe.suggest,
+#                     max_evals=50,
+#                     trials=trials)
 
-        print("Best hyperparameters:", best)
-        return best
+#         print("Best hyperparameters:", best)
+#         return best
 
-    def objective(self, params):
-        model = CTGANSynthesizer(metadata=self.metadata,
-                                 generator_dim=params['generator_dim'],
-                                 discriminator_dim=params['discriminator_dim'],
-                                 generator_lr=params['generator_lr'],
-                                 discriminator_lr=params['discriminator_lr'],
-                                 batch_size=int(params['batch_size']),
-                                 discriminator_decay=params['discriminator_decay'],
-                                 epochs=int(params['epochs']),
-                                 cuda=torch.cuda.is_available(),
-                                 pac=8)
+#     def objective(self, params):
+#         model = CTGANSynthesizer(metadata=self.metadata,
+#                                  generator_dim=params['generator_dim'],
+#                                  discriminator_dim=params['discriminator_dim'],
+#                                  generator_lr=params['generator_lr'],
+#                                  discriminator_lr=params['discriminator_lr'],
+#                                  batch_size=int(params['batch_size']),
+#                                  discriminator_decay=params['discriminator_decay'],
+#                                  epochs=int(params['epochs']),
+#                                  cuda=torch.cuda.is_available(),
+#                                  pac=8)
 
-        model.fit(self.df)
+#         model.fit(self.df)
 
-        synthetic_data = model.sample(len(self.df))
-        
-        ## need evaluation method
-        score = None
+#         synthetic_data = model.sample(len(self.df))
 
-        return {'loss': -score, 'status': STATUS_OK}
+#         ## need evaluation method
+#         score = None
+
+#         return {'loss': -score, 'status': STATUS_OK}
 
     def generate_synthetic_sample(self, synthesizer, num_rows: int = 100):
         return synthesizer.sample(num_rows=num_rows)
@@ -237,6 +252,26 @@ class SynthLeader(object):
         )
 
         return quality
+
+    def visualize_evaluation(self, synthetic_data, data_name: str, demo_cols: List):
+        from table_evaluator import TableEvaluator
+
+        eval_folder = self.figures_dir / 'evaluations' / data_name
+
+        eval_folder.mkdir(parents=True, exist_ok=True)
+
+        te = TableEvaluator(self.df, synthetic_data, cat_cols=demo_cols)
+        te.visual_evaluation(save_dir=eval_folder)
+
+        plot_list = [
+            eval_folder/'mean_std.png',
+            eval_folder/'cumsums.png',
+            eval_folder/'distributions.png',
+            eval_folder/'correlation_difference.png',
+            eval_folder/'pca.png'
+        ]
+
+        return plot_list
 
     def visualize_cumsum(self, synthetic_data, column_names: str | List, fig_name: Optional[str] = ''):
         if isinstance(column_names, str):
@@ -268,12 +303,15 @@ class SynthLeader(object):
                           title_text="Cumulative Sums of feature")
 
         if fig_name != '':
-            fig_name = f"./data/figures/{fig_name}"
+            fig_name = f"{self.figures_dir}/{fig_name}"
             pio.write_image(fig, format="png", file=fig_name)
 
         return fig
 
-    def visualize_data(self, synthetic_data, column_names: str | List, fig_name: Optional[str] = ''):
+    def visualize_data(self, synthetic_data, column_names: str | List, fig_name: Optional[str] = '', data_name: str = ''):
+
+        eval_folder = self.figures_dir / 'evaluations' / data_name
+        eval_folder.mkdir(parents=True, exist_ok=True)
 
         if isinstance(column_names, str):
             column_names = [column_names]
@@ -303,7 +341,8 @@ class SynthLeader(object):
                           300, title_text="Column Plots")
 
         if fig_name != '':
-            fig_name = f"./data/figures/{fig_name}"
+            fig_name = f"{eval_folder}/{fig_name}"
+            print("Fig name: ", fig_name)
             pio.write_image(fig, format="png", file=fig_name)
 
         return fig
